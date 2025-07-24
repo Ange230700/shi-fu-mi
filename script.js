7571df79ec29ec1a9cb579d7320ecc43a522093f
Ange230700 <kouakouangeericstephane@gmail.com>
2025-07-24 20:43:38 +0200

    ce commit écrit la logique pour obtenir le choix de l'humain

--- script.js
+++ script.js
@@ -3,16 +3,19 @@
 const gestesPossibles = ["pierre", "papier", "ciseaux"];
 
 function getComputerChoice() {
-    const indexAleatoire = Math.floor(Math.random() * gestesPossibles.length);
-    const gesteAleatoire = gestesPossibles[indexAleatoire];
-    return gesteAleatoire;
+  const indexAleatoire = Math.floor(Math.random() * gestesPossibles.length);
+  const gesteAleatoire = gestesPossibles[indexAleatoire];
+  return gesteAleatoire;
 }
 
 // console.log(getComputerChoice())
 
 function getHumanChoice() {
-    let gesteJoueur = prompt("Pierre, feuille ou ciseaux: ").toLowerCase().trim()
-    return gesteJoueur;
+  let gesteJoueur = prompt("Pierre, papier ou ciseaux: ").trim().toLowerCase();
+  if (!gestesPossibles.includes(gesteJoueur)) {
+    return getHumanChoice();
+  }
+  return gesteJoueur;
 }
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
